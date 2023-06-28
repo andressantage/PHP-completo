c161107f41887c7ca43c0bc04f84a26e2f330efa
Cesar Andres Santana <126884365+andressantage@users.noreply.github.com>
2023-06-28 13:22:28 -0500

    Add files via upload

--- 1.sql
+++ 1.sql
@@ -1,43 +1,26 @@
-mysql> CREATE TABLE pais(
-
--> idPais int NOT NULL AUTO_INCREMENT,
-
-nombrePais varchar(50) NOT NULL,
-
--> CONSTRAINT pk_pais PRIMARY KEY (idPais)
-
--> ); Query OK, 0 rows affected (0.01 sec)
-
-mysql> CREATE TABLE departamento(
-
--> idDep int NOT NULL AUTO_INCREMENT,
-
-nombreDep varchar(50) NOT NULL,
-
--> idPais int(11),
-
-CONSTRAINT pk_departamento PRIMARY KEY (idDep),
-
-->
-
-CONSTRAINT fk PaisDep FOREIGN KEY (idPais) REFERENCES pais(idPais)
-
--> );
-
-Query OK, 0 rows affected, 1 warning (0.01 sec)
-
-mysql> CREATE TABLE region(
-
-idReg int NOT NULL AUTO_INCREMENT,
-
--> nombreReg varchar(50) NOT NULL,
-
--> idDep int(11),
-
-CONSTRAINT pk_region PRIMARY KEY (idReg),
-
+CREATE TABLE pais(
+    idPais int NOT NULL AUTO_INCREMENT,
+    nombrePais varchar(50) NOT NULL,
+    CONSTRAINT pk_pais PRIMARY KEY (idPais)
 );
 
-CONSTRAINT fk_DepRegion FOREIGN KEY (idDep) REFERENCES departamento (i
+CREATE TABLE departamento(
+    idDep int NOT NULL AUTO_INCREMENT,
+    nombreDep varchar(50) NOT NULL,
+    idPais int(11),
+    CONSTRAINT pk_departamento PRIMARY KEY (idDep),
+    CONSTRAINT fk PaisDep FOREIGN KEY (idPais) REFERENCES pais(idPais)
+);
 
-Query OK, 0 rows affected, 1 warning (0.01 sec)
+CREATE TABLE region(
+    idReg int NOT NULL AUTO_INCREMENT,
+    nombreReg varchar(50) NOT NULL,
+    idDep int(11),
+    CONSTRAINT pk_region PRIMARY KEY (idReg),
+    CONSTRAINT fk_DepRegion FOREIGN KEY (idDep) REFERENCES departamento (idDep)
+);
+
+ALTER TABLE cliente ADD idRegion int(11);
+ALTER TABLE CLIENTE ADD CONSTRAINT FK_ClienteReg FOREIGN KEY (idRegion) REFERENCES region (idRegion);
+
+
